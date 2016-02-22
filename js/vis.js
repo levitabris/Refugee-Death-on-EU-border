7b651202421e21110de667062127011506c7c832
levitabris <levitabris@gmail.com>
2016-02-22 23:58:57 +0100

    all files added

---
+++ js/vis.js
@@ -0,0 +1,147 @@
+
+
+// get window size
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+var svg = d3.select("body").append("svg")
+        .attr("width", x)
+        .attr("height", y)
+        .style('background','#000043')
+        ;
+
+
+d3.select(window)
+	.on('resize', function() {
+		x = w.innerWidth || e.clientWidth || g.clientWidth;
+    	y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    
+    	svg.attr("width", x).attr("height", y);
+	});
+
+
+
+var projection = d3.geo.mercator()
+				.translate([x/2, y/2])
+				.scale(750)
+				.center([30,40])
+			;
+                       
+var path = d3.geo.path().projection(projection)
+			;
+
+
+// draw world map
+d3.json('d/world.json', function(err, world) {
+	if (err) return console.error(err);
+	var worldmap = topojson.feature(world, world.objects.worldsubunit)
+	;
+
+
+	svg.selectAll('.country').data(worldmap.features).enter()
+		.append('path')
+		.attr('stroke-width', '5') //dosen't work
+		.attr('d', path)
+		.attr('fill', '#1b246c')
+		.attr('class', function(d) { return "country " + d.properties.NAME_LONG; })
+		;
+
+
+	//draw places
+	d3.json('d/c_death.json', function(err, d) {
+			if (err) return console.error(err);
+
+			// orgin places
+			console.log();
+	  		svg	.selectAll('.originPin').data(d).enter()
+	  			.append('circle','.originPin')
+	  			.attr('r', function(d) {return Math.sqrt(d.count)})
+	  			.attr('fill', '#9093ff')
+	  			.attr('opacity', function(d){
+	  				 if (d.deathLat != "NA" && d.originLat != 'NA') {
+	  				 	return 0.2;
+	  				 }
+	  				 else {
+	  				 	return 0;
+	  				 }
+	  			})
+	  			.attr('transform',function(d) { 
+					return "translate(" + projection([
+				        d.originLng,
+				        d.originLat
+  					]) + ")";				
+	  			} )	  			
+	  			;
+
+	  		//get travel path
+			travelPath = [];
+	        for(var i=0, len=d.length-1; i<len; i++){
+	            // (note: loop until length - 1 since we're getting the next
+	            //  item with i+1)
+	            travelPath.push({
+	                type: "LineString",
+	                coordinates: [
+						[d[i].originLng, d[i].originLat],
+	  					[d[i].deathLng, d[i].deathLat]
+	                ]
+	            });
+	        };
+	  		//console.log(travelPath[4]);
+
+
+			// Standard enter / update 
+	        var pathArcs = svg.selectAll(".arc")
+	            .data(travelPath);
+
+	        //enter
+	        pathArcs.enter()
+	            .append("path").attr({
+	                'class': 'arc'
+	            }).style({ 
+	                fill: 'none',
+	            });
+
+	        //update
+	        pathArcs.attr({
+	                //d is the points attribute for this path, we'll draw
+	                //  an arc between the points using the arc function
+	                d: path
+	            })
+	            .style({
+	                stroke: '#5c64ed',
+	                'stroke-width': '0.5px',
+	                'opacity': 0.3
+	            })
+	            ;
+
+	        // exit
+	        pathArcs.exit().remove();
+
+	        // death places
+	  		svg	.selectAll('.deathPin').data(d).enter()
+	  			.append('circle','.originPin')
+	  			.attr('r', function(d) {return Math.sqrt(d.count)})
+	  			.attr('fill', '#881722')
+	  			.attr('stroke', '#FFF')
+	  			.attr('stroke-width', '0.3')
+	  			.attr('opacity', function(d){
+	  				 if (d.deathLat != "NA" && d.originLat != 'NA') {
+	  				 	return 0.5;
+	  				 }
+	  				 else {
+	  				 	return 0;
+	  				 }
+	  			})
+	  			.attr('transform', function(d) { 
+	  				return 'translate(' + projection([
+	  					d.deathLng,
+	  					d.deathLat
+	  				]) + ')';
+	  			} );
+
+	});
+});
